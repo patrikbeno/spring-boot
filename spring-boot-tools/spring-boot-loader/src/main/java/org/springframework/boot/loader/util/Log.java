9d3891b3790e44100c1c03755d6ed6a676b531d0
Patrik Beno <patrik.beno@greenhorn.sk>
2014-09-24 02:32:18 +0200

    MvnLauncher: logging

--- spring-boot-tools/spring-boot-loader/src/main/java/org/springframework/boot/loader/util/Log.java
+++ spring-boot-tools/spring-boot-loader/src/main/java/org/springframework/boot/loader/util/Log.java
@@ -2,6 +2,8 @@ package org.springframework.boot.loader.util;
 
 import org.springframework.boot.loader.mvn.MvnLauncherCfg;
 
+import java.io.PrintStream;
+
 /**
  * @author <a href="mailto:patrikbeno@gmail.com">Patrik Beno</a>
  */
@@ -9,30 +11,38 @@ public class Log {
 
 	static public void debug(String message, Object... args) {
 		if (MvnLauncherCfg.debug.asBoolean()) {
-            log("", message, args);
+            log(System.out, "", message, args);
         }
 	}
 
 	static public void info(String message, Object... args) {
         if (MvnLauncherCfg.quiet.asBoolean()) { return; }
-        log("", message, args);
+        log(System.out, "", message, args);
 	}
 
 	static public void warn(String message, Object... args) {
         if (MvnLauncherCfg.quiet.asBoolean()) { return; }
-        log("[WRN] ", message, args);
+        log(System.out, "[WRN] ", message, args);
 	}
 
-	static public void error(String message, Object... args) {
-        log("[ERR] ", message, args);
+	static public void error(Throwable thrown, String message, Object... args) {
+        System.out.flush();
+        System.err.flush();
+        log(System.err, "[ERR] ", message, args);
+        for (Throwable t = thrown ; t != null; t = t.getCause()) {
+            log(System.err, "", "Caused by: %s", t);
+        }
+        if (MvnLauncherCfg.debug.asBoolean()) {
+            thrown.printStackTrace(System.err);
+        }
 	}
 
-    static private void log(String prefix, String message, Object ... args) {
-        synchronized (System.out) {
+    static private void log(final PrintStream out, String prefix, String message, Object ... args) {
+        synchronized (out) {
             StatusLine.resetLine();
-            System.out.print(prefix);
-            System.out.printf(message, args);
-            System.out.println();
+            out.print(prefix);
+            out.printf(message, args);
+            out.println();
             StatusLine.refresh();
         }
     }
