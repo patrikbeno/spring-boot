14ac3f2cbe1c229e3220576e8bf36bfd2b88dce5
Patrik Beno <patrik.beno@greenhorn.sk>
2015-03-04 12:31:27 +0100

    MvnLauncher: update.sh

---
+++ spring-boot-tools/spring-boot-launcher/src/main/resources/org/springframework/boot/launcher/update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eu
+cd $(dirname $0)
+url=$(curl -sI 'http://greenhorn.sk/nexus/service/local/artifact/maven/redirect?r=snapshots&g=org.springframework.boot&a=spring-boot-launcher&v=1.3.0.BUILD-SNAPSHOT&e=jar' | perl -n -e '/^Location: (.*)$/ && print "$1\n"' | sed 's/\r//')
+echo $url
+wget -N $url
+[ -L springboot.jar ] && rm springboot.jar
+ln -s $(ls -1 spring-boot-launcher-*.jar | tail -n1) springboot.jar
+ls -l springboot.jar
