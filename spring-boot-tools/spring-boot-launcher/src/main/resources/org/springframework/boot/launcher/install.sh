cebadeb569acc9ae0393f45a81a580bffb394629
Patrik Beno <patrik.beno@greenhorn.sk>
2015-03-04 13:05:44 +0100

    MvnLauncher: install.sh

--- spring-boot-tools/spring-boot-launcher/src/main/resources/org/springframework/boot/launcher/install.sh
+++ spring-boot-tools/spring-boot-launcher/src/main/resources/org/springframework/boot/launcher/install.sh
@@ -6,7 +6,7 @@ mkdir -p ~/.springboot
 echo "Downloading updater..."
 wget -q -O ~/.springboot/update.sh https://raw.githubusercontent.com/patrikbeno/spring-boot/MvnLauncher/spring-boot-tools/spring-boot-launcher/src/main/resources/org/springframework/boot/launcher/update.sh
 
-echo "Updating launcher..."
+echo "Updating..."
 chmod u+x ~/.springboot/update.sh
 ~/.springboot/update.sh
 
