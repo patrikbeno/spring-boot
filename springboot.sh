dfa100de2c4c563c36df18e05ddebb28215e1b75
patrikbeno <patrikbeno@gmail.com>
2015-05-06 15:37:07 +0200

    Update springboot.sh

--- springboot.sh
+++ springboot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 file=~/.springboot/springboot.jar
-file=$(which cygpath > /dev/null && cygpath -w $file || realpath $file)
+file=$(which cygpath > /dev/null 2>&1 && cygpath -w $file || realpath $file)
 java -jar $file $*
