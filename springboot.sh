8d39ae8c4ef52bc523e29fac72eeb4b1a03608fe
patrikbeno <patrikbeno@gmail.com>
2015-08-14 09:39:22 +0200

    launcher script: support JVM and launcher options

--- springboot.sh
+++ springboot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 file=~/.springboot/springboot.jar
 file=$(which cygpath > /dev/null 2>&1 && cygpath -w $file || realpath $file)
-java -jar $file $*
+java ${JVM_OPTIONS:-} -jar $file ${SPRINGBOOT_LAUNCHER_OPTIONS:-} $*
