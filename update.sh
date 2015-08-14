a88827cb0b17521bed68ba348c562c42ba4370ed
patrikbeno <patrikbeno@gmail.com>
2015-08-14 09:42:33 +0200

    update also launcher script

--- update.sh
+++ update.sh
@@ -3,9 +3,13 @@ set -eu
 
 echo "Updating SpringBoot Launcher..."
 cd $(dirname $0)
+
 url=$(curl -sI 'http://greenhorn.sk/nexus/service/local/artifact/maven/redirect?r=snapshots&g=org.springframework.boot&a=spring-boot-launcher&v=1.3.0.BUILD-SNAPSHOT&e=jar' | perl -n -e '/^Location: (.*)$/ && print "$1\n"' | sed 's/\r//')
 wget -q -N $url
 [ -L springboot.jar ] && rm springboot.jar
 ln -s $(ls -1 spring-boot-launcher-*.jar | tail -n1) springboot.jar
 ls -l springboot.jar
 chmod 640 *.jar
+
+wget -q -N https://raw.githubusercontent.com/patrikbeno/spring-boot/dist/springboot.sh
+chmod a+x springboot.sh
