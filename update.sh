207927969aef00a70ec8d7dc4234d8849a3dd944
patrikbeno <patrikbeno@gmail.com>
2015-08-20 18:14:58 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,8 +7,8 @@ version="${2:-1.3.0.BUILD-SNAPSHOT}"
 echo "Updating SpringBoot Launcher..."
 cd $(dirname $0)
 
-url=$(curl -sI "${repo}/service/local/artifact/maven/redirect?r=snapshots&g=org.springframework.boot&a=spring-boot-launcher&v=${version}&e=jar" | perl -n -e '/^Location: (.*)$/ && print "$1\n"' | sed 's/\r//')
-wget -q -N $url
+url=$(curl -sk --head "${repo}/service/local/artifact/maven/redirect?r=snapshots&g=org.springframework.boot&a=spring-boot-launcher&v=${version}&e=jar" | perl -n -e '/^Location: (.*)$/ && print "$1\n"' | sed 's/\r//')
+curl -sk --remote-name $url
 [ -L springboot.jar ] && rm springboot.jar
 ln -s $(ls -1 spring-boot-launcher-*.jar | tail -n1) springboot.jar
 ls -l springboot.jar
