f646c804c34ce13bc0bad8d18c4c99a02c2f6f9e
patrikbeno <patrikbeno@gmail.com>
2015-08-20 12:00:21 +0200

    update repo URL + parameterize repo url + version

--- update.sh
+++ update.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -eu
 
+repo="${1:-http://repo.greenhorn.sk}"
+version="${2-:1.3.0.BUILD-SNAPSHOT}"
+
 echo "Updating SpringBoot Launcher..."
 cd $(dirname $0)
 
-url=$(curl -sI 'http://greenhorn.sk/nexus/service/local/artifact/maven/redirect?r=snapshots&g=org.springframework.boot&a=spring-boot-launcher&v=1.3.0.BUILD-SNAPSHOT&e=jar' | perl -n -e '/^Location: (.*)$/ && print "$1\n"' | sed 's/\r//')
+url=$(curl -sI "${repo}/service/local/artifact/maven/redirect?r=snapshots&g=org.springframework.boot&a=spring-boot-launcher&v=${version}&e=jar" | perl -n -e '/^Location: (.*)$/ && print "$1\n"' | sed 's/\r//')
 wget -q -N $url
 [ -L springboot.jar ] && rm springboot.jar
 ln -s $(ls -1 spring-boot-launcher-*.jar | tail -n1) springboot.jar
